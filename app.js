fe825d35734f8109d31babcbcd269e7902bd0c6b
Jeffrey Chodil <jchodilpiano@gmail.com>
2020-08-12 15:12:08 -0400

    merged branch

--- app.js
+++ app.js
@@ -4,150 +4,75 @@
 
 'use strict';
 
-const store = {
-  // 5 or more questions are required
-  questions: [
-    {
-      question: 'What color is broccoli?',
-      answers: [
-        'red',
-        'orange',
-        'pink',
-        'green'
-      ],
-      correctAnswer: 'green'
-    },
-    {
-      question: 'What is the current year?',
-      answers: [
-        '1970',
-        '2015',
-        '2019',
-        '2005'
-      ],
-      correctAnswer: '2019'
-    }
-  ],
-  quizStarted: false,
-  questionNumber: 0,
-  score: 0
-};
+function generateQuestion() { }
 
-const pageTemplates = [
-  {
-    pageName: 'frontPage',
-    content: `
-  <p class='startPara'>So begins the quiz. Test your knowledge by pressing the start button below</p>
-  <form>
-    <button type='submit' id='start'>Start Quiz</button>
-  </form>
-  `},
-  {
-    pageName: 'questionPage',
-    // TASK: make question button increment counter
-    content: `
-  <h2>Question [questions[#].[number]]<h2>
-  <img src='images/correct-answer.jpg' alt='Correct'></a>
-  <form>
-      <p>[question]</p>
-          <input name="answers" type='radio' value='answer 1'>
-            <label for='answer 1'>Answer 1</label><br>
-          <input name="answers" type='radio' value='answer 2'>
-            <label for='answer 2'>Answer 2</label><br>
-          <input name="answers" type='radio' value='answer 3'>
-            <label for='answer 3'>Answer 3</label><br>
-          <input name="answers" type='radio' value='answer 4'>
-            <label for='answer 4'>Answer 4</label><br>          
-            <button type='submit' id='checkAnswer'>Submit</button>
-          <button type='submit' id='goBack'>Previous</button>
-  </form>
-  <footer class='ticker'>
-      <p class='progress'>Question [questions[#].[number]] of [allQuestions]</p>
-      <p class='scoreCount'>[correctCount] correct out of [allQuestions]</p>
-  </footer>
-  `},
-  {
-    pageName: 'resultsPage',
-    content: `
-  <h2>Results</h2>
-  <h2 class='scoreResult'> </h2>
-  <p>[questions[0]]<br>[userAnswers[0]]</p>
-  <p>[questions[1]]<br>[userAnswers[1]]</p>
-  <p>[questions[2]]<br>[userAnswers[2]]</p>
-  <p>[questions[3]]<br>[userAnswers[3]]</p>
-  <p>[questions[4]]<br>[userAnswers[4]]</p>
-  <img src='images/trophy-two-correct.jpg' alt='Hello Kitty Guitar'>
-  <form>
-      <!--although this is a different button than the start button, it's style and function remains the same-->
-    <button type='submit' id='start'>Restart Quiz</button>
-  </form>
-  `
-  }];
-
-// temporary counter
-let counter = 0;
-
-// Use HTML template to generate desired page layout
-function generatePageHTML() {
-  let index = 0;
-  switch (counter) {
-    case 0: index = 0;
-      break;
-    case 1:
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-      index = 1;
-      break;
-    case 6: index = 2;
-      break;
-  }
-  const PageTpl = pageTemplates[index].content;
-  return PageTpl;
-}
-
-// Render page in HTML
 function renderPage() {
-  let html = generatePageHTML();
-  //console.log(html);
-  $('.js-render-page-here').html(html);  
+  // Refer back to STORE.questionNumber
+  // Before the quiz starts, render first start page
+  if (STORE.quizStarted === false) {
+    $('.js-render-page-here').html(STORE.pageTemplates[0].content);
+    // If STORE.questionNumber is within question range, render question page
+  } else if (STORE.questionNumber < STORE.questions.length) {
+    //call generateQuestion here
+    $('.js-render-page-here').html(STORE.pageTemplates[1].content);
+  } else {
+    // if store.currentQuestion === store.questions.length
+    $('.js-render-page-here').html(STORE.pageTemplates[2].content);
+  }
 }
 
-// Listening events
-// x1. front page [Submit] goes to question page
-// 2. question page [Submit] goes to next question, but remains on question template
-// x3. final question [Submit] goes to results page
-// x4. restart from results page [Submit] goes back to first page
-
-function main() {  
+function main() {
   beginQuiz();
-  checkAnswer();
+  //checkAnswer();  
 }
 
-// Will take to first question of Quiz - from front or results page
+// $('#start').on('click', function (evt){
+//   evt.preventDefault();
+  
+//   beginQuiz();
+  
+// });
+
+//Will take to first question of Quiz - from front or results page
 function beginQuiz() {
   renderPage();
-  $('#start').on('click', function (evt) {
+  $('#progress').on('click', function (evt) {
     evt.preventDefault();
-    // counter should reset to = 1    
-    console.log(counter);
-    counter++;
-    console.log(counter);
+    if(STORE.quizStarted === false){
+    STORE.quizStarted = true;
+    STORE.questionNumber++;    
     renderPage();
-  });
+    console.log(STORE.questionNumber);
+    console.log(STORE.quizStarted);
+    }else if(STORE.questionNumber < STORE.questions.length){
+      STORE.questionNumber++;    
+    renderPage();
+    console.log(STORE.questionNumber);
+    console.log(STORE.quizStarted);
+    }
+  });  
 }
 
-// Will check answer and increment counter
-function checkAnswer() {
-  $('#checkAnswer').on('click', function (evt) {
-    evt.preventDefault();
-    console.log(counter);
-    counter++;
-    console.log(counter);
-    renderPage();
-  });
-}
+// function checkAnswer() {
+//   renderPage();
+//   $('#progress').on('click', function (evt) {
+//     evt.preventDefault();    
+//     STORE.questionNumber++;    
+//     renderPage();
+//     console.log(STORE.questionNumber);
+//     console.log(STORE.quizStarted);
+//   });
+// }
+// // Will check answer and increment counter
+// function checkAnswer() {
+//   $('#checkAnswer').on('click', function (evt) {
+//     evt.preventDefault();
+//     console.log(counter);
+//     STORE.questionNumber++;
+//     console.log(counter);
+//     renderPage();
+//   });
+// }
 
 
 //function goBack() { }
--- store.js
+++ store.js
@@ -1,41 +1,116 @@
 'use strict';
 
-// Quiz Question template
-const STORE= {
-    questionCounter:1,
-    questionText:'',
-    answers:[1, 2, 3, 4],
-    correctAnswer:2,
-    playerAnswer: '',
-    correctStatus:null
-}
-
 // Quiz Questions
-const store = {
-    // 5 or more questions are required
-    questions: [
-      {
-        question: 'What color is broccoli?',
-        answers: [
-          'red',
-          'orange',
-          'pink',
-          'green'
-        ],
-        correctAnswer: 'green'
-      },
-      {
-        question: 'What is the current year?',
-        answers: [
-          '1970',
-          '2015',
-          '2019',
-          '2005'
-        ],
-        correctAnswer: '2019'
-      }
-    ],
-    quizStarted: false,
-    questionNumber: 0,
-    score: 0
-  };
+const STORE = {
+  questions: [
+    {
+      question: 'How many notes are in a Pentatonic Scale?',
+      answers: [
+        'Four',
+        'Five',
+        'Six',
+        'Eleven'
+      ],
+      correctAnswer: 'Five'
+    },
+    {
+      question: 'What instrument did Paganini play?',
+      answers: [
+        'Flute',
+        'Violin',
+        'Piano',
+        'Trumpet'
+      ],
+      correctAnswer: 'Violin'
+    },
+    {
+      question: 'Which style of music did Jimi Hendrix pioneer?',
+      answers: [
+        'Jazz Fusion',
+        'Funk',
+        'Psychedelic Rock',
+        'Jazz Bebop'
+      ],
+      correctAnswer: 'Psychedelic Rock'
+    },
+    {
+      question: 'Which of the following is NOT a rapper\'s name?',
+      answers: [
+        'Dr. Octagon',
+        'Ghostface Killah',
+        'Lil Poopy',
+        'Ice Truckah'
+      ],
+      correctAnswer: 'Ice Truckah'
+    },
+    {
+      question: 'What comes next? "Do, Re, Mi, __"',
+      answers: [
+        'Ti',
+        'Sol',
+        'Fa',
+        'La'
+      ],
+      correctAnswer: 'Fa'
+    },
+    {
+      question: 'How many keys does a piano have?',
+      answers: [
+        '88',
+        '100',
+        '52',
+        '75'
+      ],
+      correctAnswer: '88'
+    }
+  ],
+  quizStarted: false,
+  questionNumber: 0,
+  score: 0,
+  pageTemplates: [
+    {
+      pageName: 'frontPage',
+      content: `
+    <p class='startPara'>So begins the quiz. Test your knowledge by pressing the start button below</p>  
+    <button type='submit' id='progress'>Start Quiz</button>  
+    `},
+    {
+      pageName: 'questionPage',
+      content: `
+    <h2>Question [questions[#].[number]]<h2>
+    <img src='images/correct-answer.jpg' alt='Correct'></a>
+    <form>
+        <p>[question]</p>
+            <input name="answers" type='radio' value='answer 1'>
+              <label for='answer 1'>Answer 1</label><br>
+            <input name="answers" type='radio' value='answer 2'>
+              <label for='answer 2'>Answer 2</label><br>
+            <input name="answers" type='radio' value='answer 3'>
+              <label for='answer 3'>Answer 3</label><br>
+            <input name="answers" type='radio' value='answer 4'>
+              <label for='answer 4'>Answer 4</label><br>                       
+      </form>
+      <button type='submit' id='progress'>Submit</button>
+    <footer class='ticker'>
+        <p class='progress'>Question [questions[#].[number]] of [allQuestions]</p>
+        <p class='scoreCount'>[correctCount] correct out of [allQuestions]</p>
+    </footer>
+    `},
+    {
+      pageName: 'resultsPage',
+      content: `
+    <h2>Results</h2>
+    <h2 class='scoreResult'> </h2>
+    <p>[questions[0]]<br>[userAnswers[0]]</p>
+    <p>[questions[1]]<br>[userAnswers[1]]</p>
+    <p>[questions[2]]<br>[userAnswers[2]]</p>
+    <p>[questions[3]]<br>[userAnswers[3]]</p>
+    <p>[questions[4]]<br>[userAnswers[4]]</p>
+    <img src='images/trophy-two-correct.jpg' alt='Hello Kitty Guitar'>
+    
+        <!--although this is a different button than the start button, it's style and function remains the same-->
+      <button id='start'>Restart Quiz</button>
+    
+    `
+    }]
+};
